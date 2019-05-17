4282815e8685e615653ff7083743a14c79e85425
Tham <zarub2k@gmail.com>
2019-05-17 07:47:50 +0530

    small change is done

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Wire'),
+      home: MyHomePage(title: 'Flutter Wire Demo'),
     );
   }
 }
