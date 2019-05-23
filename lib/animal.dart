161bfcf4d7c37720c6f767b23b2a6f17539afafc
Tham <zarub2k@gmail.com>
2019-05-23 10:09:03 +0530

    switch case is tried with enum

--- lib/animal.dart
+++ lib/animal.dart
@@ -5,9 +5,27 @@ class Animal {
 
   Animal(this.name, this.type) {
     print('Constructor called with $name and $type');
+    printType(type);
   }
 
   factory Animal.fromProps(String name, AnimalType type) {
     return new Animal(name, type);
+  }
+
+  void printType(AnimalType type) {
+    switch(type) {
+      case AnimalType.Cat:
+        print('This is a cat');
+        break;
+      case AnimalType.Dog:
+        print('This is a dog');
+        break;
+      case AnimalType.Horse:
+        print('This is a horse');
+        break;
+      default:
+        print('Default type is cat');
+        break;
+    }
   }
 }
