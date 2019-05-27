93482a52aab9c2cc486629cec301133db838bfa1
Tham <zarub2k@gmail.com>
2019-05-27 07:25:00 +0530

    fancy button widget introduced

---
+++ lib/fancy_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FancyButton extends StatefulWidget {
+  @override
+  FancyState createState() {
+    return new FancyState();
+  }
+}
+
+class FancyState extends State<FancyButton> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
