ac1040f597bbba7bd7a451a3294a7c5a159b66d6
Tham <zarub2k@gmail.com>
2019-05-27 07:22:00 +0530

    key for widget is tried with UniqueKey()

--- lib/main.dart
+++ lib/main.dart
@@ -91,6 +91,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        key: UniqueKey(),
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
