8da5da5006511cae658083b93b7011759d4aa649
Tham <zarub2k@gmail.com>
2019-05-28 20:20:34 +0530

    Sun file is added with problem

--- lib/weather/weather.dart
+++ lib/weather/weather.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'sun.dart';
+
 class Weather extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    ColorTween tween = new ColorTween(
+      begin: Colors.red,
+      end: Colors.amber,
+    );
+    AnimationController(duration: Duration(milliseconds: 500), vsync: this);
     return Container(
-      child: Text('Weather App'),
+      child: Sun(animation: tween.animate(parent),),
     );
   }
 }
