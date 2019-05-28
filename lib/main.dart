f279f821f21057fb198c8e4e734c272f542787ba
Tham <zarub2k@gmail.com>
2019-05-28 06:49:54 +0530

    Primary theme color changed

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
 //      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
       ),
       home: MyHomePage(title: 'Flutter Wire'),
     );
