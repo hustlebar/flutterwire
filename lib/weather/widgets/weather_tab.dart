03a1bdf0fc17341118e3b2680e035ce6c3c4614d
Tham <zarub2k@gmail.com>
2019-06-02 07:15:24 +0530

    Tab bar work started

---
+++ lib/weather/widgets/weather_tab.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WeatherTabBar extends StatefulWidget {
+  @override
+  WeatherTabState createState() => WeatherTabState();
+}
+
+class WeatherTabState extends State<WeatherTabBar> {
+  @override
+  Widget build(BuildContext context) {
+    return _build(context);
+  }
+
+  TabBar _build(BuildContext context) {
+    return TabBar(
+
+    );
+  }
+}
