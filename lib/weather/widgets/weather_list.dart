cb71b2ddfe918cead59474b1a7ed787afbe07d44
Tham <zarub2k@gmail.com>
2019-06-03 07:22:41 +0530

    init state method is added to initialize cities

--- lib/weather/widgets/weather_list.dart
+++ lib/weather/widgets/weather_list.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
-class WeatherList extends StatelessWidget {
+class WeatherList extends StatefulWidget {
+  @override
+  WeatherListState createState() => WeatherListState();
+}
+
+class WeatherListState extends State<WeatherList> {
+  List<City> cities;
+  
+  @override
+  void initState() {
+    cities.add(City('Chennai', 1));
+    cities.add(City('Kovai', 10));
+    cities.add(City('Madurai', 21));
+    cities.add(City('Pondy', 11));
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return createList(context);
@@ -9,4 +26,11 @@ class WeatherList extends StatelessWidget {
   Text createList(BuildContext context) {
     return Text('Weather List');
   }
+}
+
+class City {
+  String name;
+  int popularity;
+
+  City(this.name, this.popularity);
 }
