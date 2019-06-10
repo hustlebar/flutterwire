119bdd6dd05129a3608e3c8fe9189980c31a6a21
Tham <zarub2k@gmail.com>
2019-06-10 07:02:09 +0530

    Logo Animation widget is created

---
+++ lib/weather/animation/logo_animation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LogoAnimationWidget extends StatefulWidget {
+  @override
+  LogoAnimationState createState() {
+    return null;
+  }
+}
+
+class LogoAnimationState extends State<LogoAnimationWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
