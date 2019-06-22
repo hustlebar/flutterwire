3202abae9c2a158457872262bd6d4be5961adf44
Tham <zarub2k@gmail.com>
2019-06-22 20:54:49 +0530

    Method implemented to go back

--- lib/marketplace/pages/detail.dart
+++ lib/marketplace/pages/detail.dart
@@ -3,16 +3,32 @@ import 'package:flutter/material.dart';
 class ProductDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _build();
+    return _build(context);
   }
 
-  Widget _build() {
+  Widget _build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Product details'),
       ),
 
-      body: Text('Details page'),
+      body: _buildChildren(context),
     );
+  }
+
+  Widget _buildChildren(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('Details page'),
+        RaisedButton(
+          child: Text('Go back'),
+          onPressed: () => _onBack(context),
+        )
+      ],
+    );
+  }
+
+  void _onBack(BuildContext context) {
+    Navigator.pop(context);
   }
 }
