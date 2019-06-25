1479adf535e98dfc483edf0b17fa0f31ebcd698b
Tham <zarub2k@gmail.com>
2019-06-25 10:16:45 +0530

    Gallery rendering is started

--- lib/marketplace/pages/gallery.dart
+++ lib/marketplace/pages/gallery.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutterwire/marketplace/model/product.dart';
 
 class Gallery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _build();
+    return _build(context);
   }
 
-  Widget _build() {
+  Widget _build(BuildContext context) {
     print('Gallery._build() called');
     return Scaffold(
       appBar: AppBar(
         title: Text('Gallery'),
       ),
-      body: Text('Gallery'),
+      body: _buildProducts(context),
+    );
+  }
+
+  Widget _buildProducts(BuildContext context) {
+    final List<Product> products = List<Product>.generate(
+        10,
+        (i) => Product('$i', 'Product title $i')
+    );
+    
+    ListView.builder(
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(products[index].title),
+        );
+      }
     );
   }
 }
