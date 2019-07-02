3821d2155491ec97b54ddd67830c7a3da8c0cf8c
Tham <zarub2k@gmail.com>
2019-07-02 06:30:05 +0530

    First check in

--- lib/statemgt/navigation_value.dart
+++ lib/statemgt/navigation_value.dart
@@ -23,7 +23,7 @@ class SelectionButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return RaisedButton(
       onPressed: () => _onSelection(context),
-      child: Text('Select an option'),
+      child: Text('Select your choice...'),
     );
   }
 
