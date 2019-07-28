5b69b39c9bb50411517f2100554eea9150d9c5f2
Tham <zarub2k@gmail.com>
2019-07-28 20:02:57 +0530

    factory method is written

--- lib/todo/todo.dart
+++ lib/todo/todo.dart
@@ -1,3 +1,17 @@
 class Todo {
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
 
+  Todo(this.userId, this.id, this.title, this.completed);
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      json['userId'] as int,
+      json['id'] as int,
+      json['title'] as String,
+      json['completed'] as bool
+    );
+  }
 }
