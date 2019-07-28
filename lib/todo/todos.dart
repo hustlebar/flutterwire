5752e98d1730a62432f37da069e13083c05a5156
Tham <zarub2k@gmail.com>
2019-07-28 19:53:01 +0530

    simple change

--- lib/todo/todos.dart
+++ lib/todo/todos.dart
@@ -1,5 +1,8 @@
 import 'package:http/http.dart';
+import 'dart:convert';
+
 import 'todo.dart';
+
 class TodoServices {
   Client client = new Client();
   final String url = "https://jsonplaceholder.typicode.com/todos";
@@ -7,5 +10,10 @@ class TodoServices {
   void todos() async {
     final response = await client.get(url);
     print(response.statusCode);
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> todos = jsonDecode(response.body);
+
+    }
   }
 }
