fb39abcc8d40b7530a637cd7ad73aa7e07f19de7
Tham <zarub2k@gmail.com>
2019-08-02 07:15:52 +0530

    Widget work started

--- lib/todo/todo_page.dart
+++ lib/todo/todo_page.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'todos.dart';
 
 class TodoPage extends StatefulWidget {
+  final TodoController _controller;
+  TodoPage(this._controller);
+
   @override
-  State<StatefulWidget> createState() {
-    return null;
+  _TodoPageState createState() {
+    return _TodoPageState();
   }
 }
 
 class _TodoPageState extends State<TodoPage> {
   @override
   Widget build(BuildContext context) {
+    return _build(context);
+  }
+
+  Widget _build(BuildContext context) {
     return null;
   }
 }
