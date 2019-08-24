e9dc2d08b8176138665ed9fafa751778397f3630
Tham <zarub2k@gmail.com>
2019-08-24 07:28:53 +0530

    chat message class introduced

---
+++ lib/chat/chat_message.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _build(context);
+  }
+
+  Widget _build(BuildContext ctx) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
