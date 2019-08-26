64fb04266a2c2698b93894f36d951d45481e6102
Tham <zarub2k@gmail.com>
2019-08-26 06:55:40 +0530

    children code is completed

--- lib/chat/chat_message.dart
+++ lib/chat/chat_message.dart
@@ -13,7 +13,7 @@ class ChatMessage extends StatelessWidget {
 
   Widget _build(BuildContext ctx) {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 10.0),
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
       child: Row(
         children: _buildChildren(ctx),
       ),
@@ -23,11 +23,23 @@ class ChatMessage extends StatelessWidget {
   List<Widget> _buildChildren(BuildContext ctx) {
     List<Widget> children = new List(2);
     children.add(Container(
-      margin: EdgeInsets.all(10.0),
+      margin: const EdgeInsets.all(10.0),
       child: CircleAvatar(
         child: Text(_name[0]),
       ),
     ));
+
+    children.add(Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(_name, style: Theme.of(ctx).textTheme.subhead,),
+        Container(
+          margin: const EdgeInsets.only(top: 10.0),
+          child: Text(text),
+        )
+      ],
+    ));
+
     return children;
   }
 }
