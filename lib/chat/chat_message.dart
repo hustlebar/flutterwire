7491bf2e1930555340788317f4ae768c9b388495
Tham <zarub2k@gmail.com>
2019-08-26 06:49:47 +0530

    chat message work started

--- lib/chat/chat_message.dart
+++ lib/chat/chat_message.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+
+  static const String _name = "Tham";
+
   @override
   Widget build(BuildContext context) {
     return _build(context);
@@ -10,10 +15,19 @@ class ChatMessage extends StatelessWidget {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10.0),
       child: Row(
-        children: <Widget>[
-
-        ],
+        children: _buildChildren(ctx),
       ),
     );
+  }
+
+  List<Widget> _buildChildren(BuildContext ctx) {
+    List<Widget> children = new List(2);
+    children.add(Container(
+      margin: EdgeInsets.all(10.0),
+      child: CircleAvatar(
+        child: Text(_name[0]),
+      ),
+    ));
+    return children;
   }
 }
