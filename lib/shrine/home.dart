6cf9fd17b5fedebe1ae877b9cd7cac8791db2b5d
Tham <zarub2k@gmail.com>
2019-09-09 06:55:38 +0530

    Menu action is introduced

--- lib/shrine/home.dart
+++ lib/shrine/home.dart
@@ -11,8 +11,35 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Shrine - Home'),
         leading: _buildLeading(context),
+        actions: _buildActions(context),
       ),
     );
+  }
+
+  List<Widget> _buildActions(BuildContext context) {
+    List<Widget> actions = List();
+
+    actions.add(IconButton(
+      icon: Icon(
+        Icons.search,
+        semanticLabel: 'search',
+      ),
+      onPressed: () {
+        print('Search clicked');
+      },
+    ));
+
+    actions.add(IconButton(
+      icon: Icon(
+        Icons.tune,
+        semanticLabel: 'tune',
+      ),
+      onPressed: () {
+        print('Tune clicked');
+      },
+    ));
+    
+    return actions;
   }
 
   Widget _buildLeading(BuildContext context) {
